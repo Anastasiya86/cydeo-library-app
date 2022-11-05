299a9eb9de77f667542b9d3b6d33a2b49a88bec8
anastasiya zlatanova <anastasiya8611@gmail.com>
2022-11-05 17:27:16 -0500

    hooks and pom files added

--- cars_project/src/test/java/com/cars/step_defs/Hooks.java
+++ cars_project/src/test/java/com/cars/step_defs/Hooks.java
@@ -1,2 +1,21 @@
-package com.cars.step_defs;public class Hooks {
+package com.cars.step_defs;
+
+import com.cars.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+    @After
+    public void teardownScenario(Scenario scenario){
+        //we will implement taking screenshot in this method
+        //System.out.println("It will be closing browser using cucumber @After each scenario")
+        byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        if(scenario.isFailed()) {
+            scenario.attach(screenshot, "image/png", scenario.getName());
+        }
+        Driver.closeDriver();
+    }
+
 }
